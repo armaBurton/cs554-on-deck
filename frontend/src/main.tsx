717fbf0d419e85139db1497614fd4e7defc20997
Arma Burton <armanarma@armaburton.com>
2026-01-30 18:58:21 -0800

    redoing auth

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,3 +11,6 @@ createRoot(document.getElementById("root")!).render(
     </AuthProvider>
   </StrictMode>,
 );
+
+// gcloud OAuth
+// https://console.cloud.google.com/auth/clients/909582034483-4tohqc81lm45sv3g3fd2u0njvps8d5c8.apps.googleusercontent.com?project=cloud-burton-arma
