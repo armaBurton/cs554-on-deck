2e6036d37aaee7ff4e1209ddd25d34cc13764463
Arma Burton <armanarma@armaburton.com>
2026-02-19 19:12:33 -0800

    end of class

--- v2/frontend/src/components/Profile/Dynamic/Dynamic.tsx
+++ v2/frontend/src/components/Profile/Dynamic/Dynamic.tsx
@@ -8,39 +8,42 @@ export const Dynamic: React.FC = ({
   lastName,
   stageName,
 }: ProfileProps) => {
-  const { handleProfileUpdate } = useAuth();
+  const { handleProfileUpdate, setFirstName, setLastName, setStageName } =
+    useAuth();
 
   const handleUpdate = async (e: React.SubmitEvent) => {
     e.preventDefault();
-    await handleProfileUpdate({firstName, lastName, stageName});
+    await handleProfileUpdate(firstName, lastName, stageName);
   };
   return (
     <form
-              className="profile-form update-form"
-              onSubmit={handleUpdate}
-            >
-              <h1>Profile</h1>
-              <div className="name-div">
-                <input
-                  type="text"
-                  value={firstName}
-                  placeholder={firstName ? firstName : "First Name"}
-                  className="profile-name-input first-name profile-text names"
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-                <input
-                  type="text"
-                  value={lastName}
-                  placeholder={lastName ? lastName : "Last Name"}
-                  className="profile-name-input last-name profile-text names"
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-              </div>
-              <input
-                type="text"
-                value={stageName}
-                placeholder={stageName ? stageName : "Stage Name"}
-                className="profile-name-input stage-name profile-text"
-                onChange={(e) => setStageName(e.target.value)}
-              />
-            </form>
+      className="profile-form update-form"
+      onSubmit={handleUpdate}
+    >
+      <h1>Profile</h1>
+      <div className="name-div">
+        <input
+          type="text"
+          value={firstName}
+          placeholder={firstName ? firstName : "First Name"}
+          className="profile-name-input first-name profile-text names"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={lastName}
+          placeholder={lastName ? lastName : "Last Name"}
+          className="profile-name-input last-name profile-text names"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+      <input
+        type="text"
+        value={stageName}
+        placeholder={stageName ? stageName : "Stage Name"}
+        className="profile-name-input stage-name profile-text"
+        onChange={(e) => setStageName(e.target.value)}
+      />
+    </form>
+  );
+};
